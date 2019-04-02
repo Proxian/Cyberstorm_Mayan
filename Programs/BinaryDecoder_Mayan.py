98e35547f536f8ba7b14fe9b9a75be1428c0131a
Proxian <37815980+Proxian@users.noreply.github.com>
2019-04-02 15:05:00 -0500

    Fixed formating
    Fixed the formating

---
+++ Programs/BinaryDecoder_Mayan.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+
+ ####################################################################
+#                                                                   #
+#   Authors: Justin Berthelot, Dawson Markham, Matthew Eldrige,     #
+#   Ryan Utley, Caleb Snook, Emily Rumfola, Valentin Le Gall        #
+#   Github link: https://github.com/Proxian/Cyberstorm_Mayan/       #
+#                                                                   #
+#   Date: 03 / 21 / 19                                              #
+#   Description: Translates 7-bit and 8-bit binary to ASCII         #
+#                                                                   #
+####################################################################
+
+### LIBRARIES ###
+from sys import stdout
+
+
+
+### FUNCTIONS ###
+
+# Determines the bit-type for binary input
+def BinBitType(binary_message):
+    # Get the size of the message
+    size = len(binary_message)
+    # Only use numeric values
+    if binary_message.isdigit():
+        # Type 1 - Either 7-bit or 8-bit
+        if not ((size+7) % 7) and not ((size+8) % 8):
+            return 1;
+        # Type 2 - 7-bit
+        elif not ((size+7) % 7):
+            return 2;
+        # Type 3 - 8-bit
+        elif not ((size+8) % 8):
+            return 3;
+        # Unknown bit; treat as error
+        else:
+            print "Not a recoginizable binary number!"
+            exit(1)
+    # Non-number value; treat as error
+    else:
+        print "Not a numeric value!"
+        exit(1)
+
+# 8-bit binary conversion
+def Bin8Bit(data):
+    # Loops over the data in 8-bit increments
+    for i in range(0,len(data),8):
+        # prints the ASCII equivalent character
+        stdout.write(chr(int(data[i:i+8], 2)))
+        
+# 7-bit binary conversion
+def Bin7Bit(data):
+    # Loops over the data in 7-bit increments
+    for i in range(0,len(data),7):
+        # prints the ASCII equivalent character
+        stdout.write(chr(int(data[i:i+7], 2)))
+        
+# Either 7-bit or 8-bit binary
+#  (prints both)
+def BinNBit(data):
+    
+    Bin7Bit(data)
+    # Add a newline for clean formatting
+    stdout.write('\n')
+    Bin8Bit(data)
+
+# Handles user input and data direction
+def Main():
+    # Get user input
+    binary_data = raw_input()
+    # Determine binary bit-number
+    bin_type = BinBitType(binary_data)
+
+    # Either 7-bit or 8-bit binary
+    #  (prints both)
+    if bin_type == 1:
+        BinNBit(binary_data)
+        
+    # 7-bit binary
+    elif bin_type == 2:
+        Bin7Bit(binary_data)
+
+    # 8-bit binary
+    else:
+        Bin8Bit(binary_data)
+
+
+### MAIN ###
+Main()
--- Programs/fetch_Markham.py
+++ Programs/fetch_Markham.py
@@ -10,12 +10,12 @@
 #		a binary decoder.
 ########################################################################
 
-# Import libraries
+#Import libraries
 from ftplib import FTP
 import sys
 
 # CONTROL VARIABLES
-mode = "7" 			# Modes are: 7 or 10
+mode = "10" 			# Modes are: 7 or 10
 server = "jeangourd.com"	# FTP server
 ftp_user = "anonymous"		# Username
 
@@ -24,30 +24,29 @@ ftp_user = "anonymous"		# Username
 ###############
 
 # 7-Bit Decoder
-def Bit_7():
-        # Set an empty string
-	decoded = ""
+def bit_7():
+    #Set an empty string
+    decoded = ""
 
-        # Go through input in groups of seven characters, convert to appropiate character and add to string
-        for i in range(0, len(cipher), 7):
-                temp =  cipher[i:i+7]
-		decoded = decoded + chr(int(temp, base=2))
-
-	# Display decoded string
-	print decoded
+    # Go through input in groups of seven characters, convert to appropiate character and add to string
+    for i in range(0, len(cipher), 7):
+        temp = cipher[i:i+7]
+        decoded = decoded + chr(int(temp, base=2))
+    # Display decoded string
+    print decoded
 
 # 8-Bit Decoder
-def Bit_8():
-        # set an empty string
-        decoded = ""
+def bit_8():
+    # set an empty string
+    decoded = ""
 
-	# Go through input in groups of 8 characters, convert to appropriate character and add to string
-        for i in range(0, len(cipher), 8):
-                temp =  cipher[i:i+8]
-                decoded = decoded + chr(int(temp, base=2))
+    # Go through input in groups of 8 characters, convert to appropriate character and add to string
+    for i in range(0, len(cipher), 8):
+        temp =  cipher[i:i+8]
+        decoded = decoded + chr(int(temp, base=2))
 
-	# Display decoded string
-	print decoded
+    # Display decoded string
+    print decoded
 
 ###############
 #    MAIN     #
@@ -59,15 +58,15 @@ ftp.login(user=ftp_user)
 
 # Use mode to go to specified directory
 if (mode == "7"):
-	ftp.cwd("7")
+    ftp.cwd("7")
 
 elif (mode == "10"):
-	ftp.cwd("10")
+    ftp.cwd("10")
 
 else:
-	print "Incorrect mode: Please use the character 7 or the character 10"
-	ftp.quit()
-	exit
+    print "Incorrect mode: Please use the character 7 or the character 10"
+    ftp.quit()
+    exit
 
 # Create list of files
 files = []
@@ -78,41 +77,41 @@ cipher = ""
 
 # Convert the permissions of each file to binary
 for i in range (0, len(files)):
-	binary = ""		# Reset as empty string
+    binary = ""		# Reset as empty string
 
-	# Go through the  first 10 character of the line (the permissions
-	for j in range(0,10):			
-		if (files[i][j] != '-'):	# If not '-', add 1 
-			binary = binary + '1'
-		else:				# Add 0 otherwise
-			binary = binary + '0'
-	
-	# If mode is 7-Bit, append the last 7 bits to cipher if the first three bits do not contain a 1
-	if (mode == '7'):
-		if (binary[0] != '1' and  binary[1] != '1' and binary[2] != '1'):
-			cipher = cipher + binary[3:]
-	# Else, add 10-bit binary to cipher
-	else:
-		cipher = cipher + binary
+    # Go through the  first 10 character of the line (the permissions
+    for j in range(0,10):
+        if (files[i][j] != '-'):	# If not '-', add 1
+            binary = binary + '1'
+        else:				# Add 0 otherwise
+            binary = binary + '0'
 
+    # If mode is 7-Bit, append the last 7 bits to cipher if the first three bits do not contain a 1
+    if (mode == '7'):
+        if (binary[0] != '1' and  binary[1] != '1' and binary[2] != '1'):
+            cipher = cipher + binary[3:]
+    # Else, add 10-bit binary to cipher
+    else:
+        cipher = cipher + binary
+    print cipher
 # Close FTP connection
-ftp.quit()	
+ftp.quit()
 
 # Check if cipher contains a multiple of 7 or 8 bits
 if ((len(cipher) % 7) == 0 or (len(cipher) % 8) == 0 ):
-	# Check if 7-Bit Decoder can be used
-	if ((len(cipher) % 7) == 0 ):
-        	Bit_7()
+    # Check if 7-Bit Decoder can be used
+    if ((len(cipher) % 7) == 0 ):
+        bit_7()
 
-	# Check if 8-Bit Decoder can be used
-	if ((len(cipher) % 8) == 0 ):
-		Bit_8()
+    # Check if 8-Bit Decoder can be used
+    if ((len(cipher) % 8) == 0 ):
+        bit_8()
 
 # Else, use both decoders
 else:
-	print "7-Bit:"
-	Bit_7()
-	print "~~~~~~~~~~~~~~~~~~~~~~~~"
-	print "8-Bit:"
-	Bit_8()
+    print "7-Bit:"
+    bit_7()
+    print "~~~~~~~~~~~~~~~~~~~~~~~~"
+    print "8-Bit:"
+    bit_8()
 
