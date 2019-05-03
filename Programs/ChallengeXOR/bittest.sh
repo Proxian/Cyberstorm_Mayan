b0955b7f02c789a862e8a4ee4d867a0675751e12
JustinBerthelot <37705166+JustinBerthelot@users.noreply.github.com>
2019-05-03 12:05:45 -0500

    fff

--- Programs/ChallengeXOR/bittest.sh
+++ Programs/ChallengeXOR/bittest.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Delete old folder if it exists and create new one
-rm -rf stegtest
-mkdir stegtest
+rm -rf steg2
+mkdir steg2
 
 # $i represents the interval
 for i in {1..4}
 do
 	# $j represents the offset
-	for j in {0..10}
+	for j in {1..10}
 	do
-		Interval=$i
+		Interval=$(($i*2))
 		# Set up the file name
-		FileName="./stegtest/Bit$Interval"
+		FileName="./steg2/Bit$Interval"
 		offset=$((2**$j+1))
 		Temp="_o$offset"
 		FileName="$FileName$Temp"
--- Programs/ChallengeXOR/bytetest.sh
+++ Programs/ChallengeXOR/bytetest.sh
@@ -2,35 +2,35 @@
 
 
 # Remove old folder and create new folder for output files
-rm -rf stegtest
-mkdir stegtest
+rm -rf steg2
+mkdir steg2
 
 # Use $i for interval
-for i in {0..8}
+for i in {0..10}
 do
 	# interval
 	Interval=$((2**$i))
 	# Filename
-	FileName="./stegtest/Byte$Interval"
-	Temp="_o$2"
+	FileName="./steg2/Byte$Interval"
+	Temp="_o2"
 	FileName="$FileName$Temp"
 	# echo so we know status
 	echo $FileName
 	# Run the Steg Program
-	python steg.py -B -r -o$j -i$Interval -w$1 > $FileName
+	python steg.py -B -r -o256 -i$Interval -w$1 > $FileName
 done
 
 # Use $i for interval
-for i in {0..8}
+for i in {0..10}
 do
 	# interval
 	Interval=$((2**$i))
 	# Filename
-	FileName="./stegtest/Byte$Interval"
-	Temp="_o$2048"
+	FileName="./steg2/Byte$Interval"
+	Temp="_o2048"
 	FileName="$FileName$Temp"
 	# echo so we know status
 	echo $FileName
 	# Run the Steg Program
-	python steg.py -B -r -o$j -i$Interval -w$1 > $FileName
+	python steg.py -B -r -o2048 -i$Interval -w$1 > $FileName
 done
